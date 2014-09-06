0b9d393240d7f9e9ecaddc844a53caa5f989d856
Firehead94 <jjonscott@comcast.net>
2014-09-06 05:39:20 -0400

    Utils and Base Item

--- src/main/java/com/fire/flamecore/items/BaseItem.java
+++ src/main/java/com/fire/flamecore/items/BaseItem.java
@@ -1,5 +1,35 @@
 package com.fire.flamecore.items;
 
-public class BaseItem {
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
+import com.fire.flamecore.ModSettings;
+
+public class BaseItem extends Item 
+{
+
+	public BaseItem() 
+	{
+		super();
+		maxStackSize = 64;
+		setCreativeTab(CreativeTabs.tabMaterials);
+	}
+	
+	@Override
+	public String getUnlocalizedName()
+	{
+		return String.format("item.%s%s", ModSettings.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+	}
+	
+	@Override
+	public String getUnlocalizedName(ItemStack itemstack)
+	{
+		return String.format("item.%s%s", ModSettings.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+	}
+	
+	protected String getUnwrappedUnlocalizedName(String unlocalName)
+	{
+		return unlocalName.substring(unlocalName.indexOf(".") + 1);
+	}
 }
