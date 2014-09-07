c1791b4a4deb393f1f545a7a0ce0f1c50f4aa391
Firehead94 <jjonscott@comcast.net>
2014-09-06 21:20:46 -0400

    More base files

--- src/main/java/com/fire/flamecore/blocks/BaseBlock.java
+++ src/main/java/com/fire/flamecore/blocks/BaseBlock.java
@@ -1,5 +1,50 @@
 package com.fire.flamecore.blocks;
 
-public class BaseBlock {
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+import com.fire.flamecore.ModSettings;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@GameRegistry.ObjectHolder(ModSettings.MOD_ID)
+public class BaseBlock extends Block 
+{
+	public final String DEFAULT = "DEFAULT NAME";
+
+	public BaseBlock()
+	{
+		super(Material.rock);
+		this.setBlockName(DEFAULT);
+		this.setBlockTextureName(super.getUnlocalizedName());
+	}
+	
+	public BaseBlock(Material mat) 
+	{
+		super(mat);
+		this.setBlockName(DEFAULT);
+		this.setBlockTextureName(super.getUnlocalizedName());
+	}
+	
+	@Override
+	public String getUnlocalizedName()
+	{
+		return String.format("tile.%s%s", ModSettings.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+	}
+	
+	protected String getUnwrappedUnlocalizedName(String unlocalName)
+	{
+		return unlocalName.substring(unlocalName.indexOf(".") + 1);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister iconRegister)
+	{
+		blockIcon = iconRegister.registerIcon(String.format("%s", this.getUnwrappedUnlocalizedName(this.getUnlocalizedName()))); 
+	}
 
 }
--- src/main/java/com/fire/flamecore/items/BaseItem.java
+++ src/main/java/com/fire/flamecore/items/BaseItem.java
@@ -1,11 +1,17 @@
 package com.fire.flamecore.items;
 
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import com.fire.flamecore.ModSettings;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@GameRegistry.ObjectHolder(ModSettings.MOD_ID)
 public class BaseItem extends Item 
 {
 
@@ -31,5 +37,12 @@ public class BaseItem extends Item
 	protected String getUnwrappedUnlocalizedName(String unlocalName)
 	{
 		return unlocalName.substring(unlocalName.indexOf(".") + 1);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister iconRegister)
+	{
+		itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1)); 
 	}
 }
